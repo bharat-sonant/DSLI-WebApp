eb8c08b54ffcc52e01ea2131e25d02d33bf1ea50
hsc-soft <hsc.wevois@gmail.com>
2021-08-19 16:18:33 +0530

    committed

--- Code/src/app/home-page/home-page.component.ts
+++ Code/src/app/home-page/home-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit,VERSION } from '@angular/core';
-import { PdfViewerModule } from 'ng2-pdf-viewer'; // <- import PdfViewerModule
 
 @Component({
   selector: 'app-home-page',
