96cd004e8a2489242bf327a5f4b11c269314f869
Nikhil27 <nikhilkhandelwal125@gmail.com>
2021-09-17 10:50:52 +0530

    committed.

--- Code/src/environments/environment.prod.ts
+++ Code/src/environments/environment.prod.ts
@@ -3,10 +3,11 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyCk24Av5B9UKkX9WnIMSc1aIhj2HogEXj8",
   authDomain: "dsiapp-103c4.appspot.com",
-  databaseURL: "https://dsiapp-103c4.firebaseio.com/",
+  databaseURL: "https://dsiapp-103c4-acafb.firebaseio.com/",
   projectId: "dsiapp-103c4",
   storageBucket: "gs://dsiapp-103c4.appspot.com",
   // messagingSenderId: "240441066397",
   appId: "1:427232668165:android:d3bccd511641c5ac3c0a6c",
+  
   }
 };
--- Code/src/environments/environment.ts
+++ Code/src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  /*
   firebase: {
     apiKey: "AIzaSyCk24Av5B9UKkX9WnIMSc1aIhj2HogEXj8",
   authDomain: "dsiapp-103c4.appspot.com",
@@ -12,6 +13,20 @@ export const environment = {
   storageBucket: "gs://dsiapp-103c4.appspot.com",
   // messagingSenderId: "240441066397",
   appId: "1:427232668165:android:d3bccd511641c5ac3c0a6c",
+
+  }
+
+*/
+  // testing
+
+  firebase: {
+    apiKey: "AIzaSyCk24Av5B9UKkX9WnIMSc1aIhj2HogEXj8",
+    authDomain: "dsiapp-103c4.appspot.com",
+    databaseURL: "https://dsiapp-103c4-acafb.firebaseio.com/",
+    projectId: "dsiapp-103c4",
+    storageBucket: "gs://dsiapp-103c4.appspot.com",
+    // messagingSenderId: "240441066397",
+    appId: "1:427232668165:android:d3bccd511641c5ac3c0a6c",
 
   }
 };
